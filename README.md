b780a4f861390a1433cc6b5d690c28f294ede718
Nicholas Dupuis <nicholas.dupuis@ge.com>
2017-01-19 08:52:05 -0600

    Add updatebranches.sh

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ A collection of miscellaneous scripts I use while developing.
 #### getbranchnames.sh
 
 I often work on projects with nested git repositories.  While developing on feature branches on different modules, it can be easy to lose track.  When this script is run in a folder containing multiple repositories, it will print out what branch name each repository is currently on.
+
+#### updatebranches.sh
+
+When run in a folder containing multiple git repositories, this script will checkout the "dev" branch and pull the latest changes.
---
+++ scripts/updatebranches.sh
@@ -0,0 +1,19 @@
+for dir in $(ls)
+  do
+    cd $dir 2>>/dev/null
+    if [[ -e ./.git ]]
+      then
+        echo "Updating "$dir
+        git stash > /dev/null 2>&1        #Stash any changes there might be
+        git checkout dev > /dev/null 2>&1 #Go to the dev branch
+        git pull -p > /dev/null 2>&1      #Pull latest, prune old branches
+    fi
+    cd ..
+  done
+
+  # TODO: Maybe store what branch the user was previously on, switch to previous
+  # branch and pop changes from the stash.
+  #
+  # Figure out a better way to do error handling
+  #
+  # Maybe have an option to rebase the current branch on dev
