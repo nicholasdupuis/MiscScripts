c3c088448cf5e6051ffe1522038229ad34cdcff6
Nicholas Dupuis <nicholas.dupuis@ge.com>
2017-01-19 08:47:41 -0600

    Add getbranchnames.sh

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-#### MiscScripts
+### MiscScripts
 
 A collection of miscellaneous scripts I use while developing.
+
+#### getbranchnames.sh
+
+I often work on projects with nested git repositories.  While developing on feature branches on different modules, it can be easy to lose track.  When this script is run in a folder containing multiple repositories, it will print out what branch name each repository is currently on.
---
+++ scripts/getbranchnames.sh
@@ -0,0 +1,17 @@
+red() { echo "$(tput setaf 1)$*$(tput setaf 7)"; }
+green() { echo "$(tput setaf 2)$*$(tput setaf 7)"; }
+white() { echo "$(tput setaf 7)$*$(tput setaf 7)"; }
+
+for dir in $(ls)
+do
+  if [[ -d ./$dir ]]
+    then
+        cd $dir
+        branch_name=$(git symbolic-ref --short -q HEAD)
+        #Prints "module_name: branch_name" with the branch name in green.
+        echo $(white $dir)": "$(green $branch_name)
+        cd ..
+    else
+        red $dir" is not a directory"
+  fi
+done
