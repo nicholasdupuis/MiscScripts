c70462b103cd09de234732c5e64f43d41c33b69d
Nicholas Dupuis <nicholas.dupuis@ge.com>
2017-01-23 12:43:17 -0600

    Added some error handling

--- scripts/updatebranches.sh
+++ scripts/updatebranches.sh
@@ -1,21 +1,18 @@
-#!/usr/bin/env bash
-
-for dir in $(ls)
+for dir in $(ls)                            #For each directory
   do
-    cd $dir 2>>/dev/null
-    if [[ -e ./.git ]]
+    cd $dir 2>>/dev/null                    #cd into directory, hide errors
+    if [[ -e ./.git ]]                      #If this is a git directory...
       then
         echo "Updating "$dir
-        git stash > /dev/null 2>&1        #Stash any changes there might be
-        git checkout dev > /dev/null 2>&1 #Go to the dev branch
-        git pull -p > /dev/null 2>&1      #Pull latest, prune old branches
+        git stash > /dev/null 2>&1          #Stash any changes there might be
+        git checkout dev > /dev/null 2>&1   #Go to the dev branch
+          if [[ $? -eq 0 ]]                 #If checkout is successful
+            then
+              git pull -p > /dev/null 2>&1  #Update the dev branch, prune old branches
+            else
+              git checkout master /dev/null 2>&1
+              git pull -p
+          fi
     fi
     cd ..
   done
-
-  # TODO: Maybe store what branch the user was previously on, switch to previous
-  # branch and pop changes from the stash.
-  #
-  # Figure out a better way to do error handling
-  #
-  # Maybe have an option to rebase the current branch on dev
