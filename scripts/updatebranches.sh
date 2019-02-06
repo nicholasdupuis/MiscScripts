ee7ef82fe687a62dcf2124103d5c058ec7c439af
Nicholas Dupuis <ndupuis08@gmail.com>
2019-02-06 11:18:36 -0600

    Remove duplicated code to improve readability

--- scripts/updatebranches.sh
+++ scripts/updatebranches.sh
@@ -6,30 +6,32 @@
 # cd into a given directory and update dev branch, or master branch depending on the repo
 update_repo()
 {
-    cd $1 2>>/dev/null                               
-        if [[ -d .git ]]                             # If this is a git directory...
+    cd $1                            
+        if [[ -d .git ]]              # If this is a git directory...
         then
-            git stash > /dev/null 2>&1               # Stash any changes there might be
-            git checkout dev > /dev/null 2>&1        # Go to the dev branch
-            if [[ $? -eq 0 ]]                        # If checkout is successful
+            git stash                 # Stash any changes there might be
+            git checkout dev          # Go to the dev branch
+            if [[ $? -eq 0 ]]         # If checkout is successful
                 then
-                git pull -p > /dev/null 2>&1         # Update the dev branch, prune old branches
+                git pull -p           # Update the dev branch, prune old branches
                 else
-                git checkout master > /dev/null 2>&1
-                git pull -p > /dev/null 2>&1
+                git checkout master   # Checkout master if dev does not exist
+                git pull -p
             fi
-            echo "$dir updated."
             cd ..
         fi
-}
+} &> /dev/null
 
 #
 # Loop through the modules directory, spawning a new thread for each update, and wait
 # for all child processes to complete before finishing.
 # 
 echo "Updating repositories..."
+
 for dir in $(ls)
   do
     update_repo $dir &
   done
   wait
+
+echo "Done!"
